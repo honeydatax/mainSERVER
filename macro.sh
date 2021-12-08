f7252230495f7da226c13ec8fbd6c13dc3909ba9
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-08 16:59:58 +0000

    Update macro.sh

--- macro.sh
+++ macro.sh
@@ -1,2 +1,1 @@
-curl https://raw.githubusercontent.com/honeydatax/elearning/main/$1.macro > /tmp/macro
-sh < /tmp/macro
+curl https://raw.githubusercontent.com/honeydatax/elearning/main/$1.macro | sh
